f299a23acc6f1703f473b8d67804f41437b111c8
thomashagstrom@outlook.com <thomashagstrom@outlook.com>
2018-03-28 14:15:59 +0200

    React-Native-Calendar typings added (d.ts). Updated snapshots #2

---
+++ source/@types/react-native-calendars/index.d.tsx
@@ -0,0 +1,14 @@
+declare module 'react-native-calendars' {
+  import * as React from 'react';
+  import { View } from 'react-native';
+
+  export interface ICalendarProps {
+    style: React.CSSProperties;
+    items: any;
+  }
+
+  export class Calendar extends React.Component<ICalendarProps, {}> {}
+  export class CalendarList extends React.Component<ICalendarProps, {}> {}
+  export class Agenda extends React.Component<ICalendarProps, {}> {}
+  export const LocaleConfig: React.Component;
+}
